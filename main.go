2211d70d68d92a36804bdc0e6c4ec8f1df801d9b
ilay goldberg <ilay.gold123@gmail.com>
2024-10-25 16:44:58 +0300

    feat: add ability to fetch file from URL

--- main.go
+++ main.go
@@ -1,29 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"image"
 	"log"
 
 	"github.com/alexflint/go-arg"
 )
 
-func main() {
+func handle_user_input() image.Image {
 	var args struct {
-		Image string `arg:"positional"`
+		URL       string
+		ImagePath string `arg:"positional"`
+	}
+	arg.MustParse(&args)
+
+	if args.URL != "" && !file_exists(args.ImagePath) {
+		get_image_from_url(args.URL, args.ImagePath)
 	}
 
-	arg.MustParse(&args)
-	fmt.Println("Input:", args.Image)
-
-	screen_width, screen_height := get_screen_size()
-	screen_height -= 2 // for extra lines
-	// clear_screen()
-
-	img, err := read_jpeg_image(fmt.Sprintf("test-images/%s", args.Image))
+	img, err := read_jpeg_image(args.ImagePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	return img
+}
+
+func main() {
+	img := handle_user_input()
+
+	screen_width, screen_height := get_screen_size()
+	screen_height -= 2 // for extra lines
+	// clear_screen()
 	pixelated_image := pixelate_image(img, screen_width, screen_height)
 
 	display_image(pixelated_image)
--- utils.go
+++ utils.go
@@ -1,21 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/jpeg"
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
+var IMAGE_FOLDER string = "images"
+
+func file_exists(filename string) bool {
+	_, err := os.Stat(fmt.Sprintf("%s/%s.jpg", IMAGE_FOLDER, filename))
+	if err == nil {
+		return true // File exists
+	}
+	if os.IsNotExist(err) {
+		return false // File does not exist
+	}
+	return false // Error in checking file
+}
 func read_jpeg_image(filename string) (image image.Image, error error) {
-	// Open the image file
-	file, err := os.Open(filename) // Replace with your image file path
+	file, err := os.Open(fmt.Sprintf("%s/%s.jpg", IMAGE_FOLDER, filename))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	// Decode the image (supports JPEG, PNG, etc., based on import)
 	img, err := jpeg.Decode(file)
 	return img, err
 }
@@ -30,4 +43,24 @@ func save_jpeg_image(img image.Image, filename string) {
 		log.Fatal(err)
 		os.Exit(1)
 	}
+}
+
+func get_image_from_url(url string, image_name string) {
+	response, e := http.Get(url)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer response.Body.Close()
+
+	file, err := os.Create(fmt.Sprintf("%s/%s.jpg", IMAGE_FOLDER, image_name))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Success!")
 }
